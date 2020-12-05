e18edf901921cb0364615ea8cba14dc3442b6176
Michael Ballantyne <michael.ballantyne@gmail.com>
2020-12-05 08:44:35 -0700

    test effect trampoline in splicing-let

--- test/effect-trampoline.rkt
+++ test/effect-trampoline.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
-(require (for-syntax syntax/parse "../effect-trampoline.rkt"))
+(require (for-syntax syntax/parse "../effect-trampoline.rkt")
+         racket/splicing)
 
 (define-syntax test-def
   (effect-trampoline-macro
@@ -9,6 +10,12 @@
       (trampoline-lift! #'(define-syntax name rhs))
       (define val (syntax-local-value #'name))
       (displayln (val #'(_ 5)))
+      (trampoline-lift! #'(name 5))
       #'(name 5)])))
 
 (test-def foo (syntax-parser [(_ arg) #'(+ arg 1)]))
+(foo 7)
+
+(splicing-let ([x 10])
+  (test-def bar (syntax-parser [(_ arg) #'(+ arg 1)]))
+  (bar x))

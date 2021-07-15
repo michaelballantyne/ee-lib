ade1fdeece880f4c79c1ea4f76a8a626721e3934
Michael Ballantyne <michael.ballantyne@gmail.com>
2021-07-14 22:02:12 -0600

    add drracket as a build dep for docs

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define name "ee-lib")
 (define deps '("base" "rackunit-lib"))
-(define build-deps '("racket-doc" "scribble-lib"))
+(define build-deps '("racket-doc" "scribble-lib" "drracket"))
 (define scribblings '(("scribblings/ee-lib.scrbl" () (experimental))))
 (define compile-omit-paths '("test/node_modules"))
 (define test-omit-paths '("test/node_modules"))

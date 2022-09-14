41b0d07f2ed59bf3674ed8548e9f7cf48a423121
Michael Ballantyne <michael.ballantyne@gmail.com>
2022-09-14 14:28:33 -0400

    errors for non-module context

--- define.rkt
+++ define.rkt
@@ -15,27 +15,28 @@
 (begin-for-syntax
   (define-syntax-class symbol
     (pattern stx:id
-             #:attr sym (syntax-e (attribute stx)))))
+      #:attr sym (syntax-e (attribute stx)))))
 
 (define-syntax define-literal-forms
-  (syntax-parser
-    [(_ literal-set-name:id
-        (~optional (~seq #:syntax-class syntax-class-name:id))
-        (~optional (~seq #:binding-space (~or space:symbol #f)))
-        msg:expr (name:id ...))
-     #:with (spaced-name ...) (map (in-space (attribute space.sym)) (attribute name))
-     #'(begin
-         (define-for-syntax (expand-to-error stx)
-           (raise-syntax-error #f msg stx))
-         (define-syntax spaced-name expand-to-error)
-         ...
-         (begin-for-syntax
-           (define-literal-set literal-set-name
-             (spaced-name ...))
-           (~? (define-syntax-class syntax-class-name
-                 #:literal-sets (literal-set-name)
-                 (pattern (~or spaced-name ...)))
-               (begin))))]))
+  (module-macro
+   (syntax-parser
+     [(_ literal-set-name:id
+         (~optional (~seq #:syntax-class syntax-class-name:id))
+         (~optional (~seq #:binding-space (~or space:symbol #f)))
+         msg:expr (name:id ...))
+      #:with (spaced-name ...) (map (in-space (attribute space.sym)) (attribute name))
+      #'(begin
+          (define-for-syntax (expand-to-error stx)
+            (raise-syntax-error #f msg stx))
+          (define-syntax spaced-name expand-to-error)
+          ...
+          (begin-for-syntax
+            (define-literal-set literal-set-name
+              (spaced-name ...))
+            (~? (define-syntax-class syntax-class-name
+                  #:literal-sets (literal-set-name)
+                  (pattern (~or spaced-name ...)))
+                (begin))))])))
 
 (require (for-syntax syntax/parse/private/sc))
 
@@ -47,9 +48,9 @@
 
   (define-syntax-class head
     (pattern (name:id . rest)
-             #:attr pat #'((~var name id) . rest))
+      #:attr pat #'((~var name id) . rest))
     (pattern name:id
-             #:attr pat #'(~var name id)))
+      #:attr pat #'(~var name id)))
 
   (define (make-simple-macro-definition-transformer define-form)
     (syntax-parser
@@ -60,22 +61,23 @@
                          [h.pat . body]))])))
 
 (define-syntax define-extensible-syntax
-  (syntax-parser
-    [(_ name)
-     (def/stx gen-name (format-id #'name "gen:~a" #'name))
-     (def/stx name-transform (format-id #'name "~a-transform" #'name))
-     (def/stx name-rep (format-id #'name "~a-rep" #'name))
-     (def/stx name-rep-procedure (format-id #'name "~a-rep-procedure" #'name))
-     (def/stx define-name (format-id #'name "define-~a" #'name))
-     (def/stx define-simple-name (format-id #'name "define-simple-~a" #'name))
-     #'(begin
-         (begin-for-syntax
-           (define-generics name
-             (name-transform name stx))
-           (struct name-rep (procedure)
-             #:methods gen-name
-             [(define (name-transform s stx)
-                ((name-rep-procedure s) stx))]))
-         (define-syntax define-name (make-extension-definition-transformer #'name-rep))
-         (define-syntax define-simple-name
-           (make-simple-macro-definition-transformer #'define-name)))]))
+  (module-macro
+   (syntax-parser
+     [(_ name)
+      (def/stx gen-name (format-id #'name "gen:~a" #'name))
+      (def/stx name-transform (format-id #'name "~a-transform" #'name))
+      (def/stx name-rep (format-id #'name "~a-rep" #'name))
+      (def/stx name-rep-procedure (format-id #'name "~a-rep-procedure" #'name))
+      (def/stx define-name (format-id #'name "define-~a" #'name))
+      (def/stx define-simple-name (format-id #'name "define-simple-~a" #'name))
+      #'(begin
+          (begin-for-syntax
+            (define-generics name
+              (name-transform name stx))
+            (struct name-rep (procedure)
+              #:methods gen-name
+              [(define (name-transform s stx)
+                 ((name-rep-procedure s) stx))]))
+          (define-syntax define-name (make-extension-definition-transformer #'name-rep))
+          (define-syntax define-simple-name
+            (make-simple-macro-definition-transformer #'define-name)))])))
